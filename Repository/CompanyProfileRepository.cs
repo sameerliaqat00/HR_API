e79d792c82c7294cc7e95c49a4207a8599d63538
Sameer Liaqat <59957710+sameerliaqat00@users.noreply.github.com>
2023-02-08 13:56:17 +0500

    Make Generic Repository

--- Repository/CompanyProfileRepository.cs
+++ Repository/CompanyProfileRepository.cs
@@ -6,58 +6,18 @@ using System.Linq.Expressions;
 
 namespace HR_API.Repository
 {
-    public class CompanyProfileRepository : ICompanyProfileRepository
+    public class CompanyProfileRepository : Repository<CompanyProfile>, ICompanyProfileRepository
     {
         private readonly ApplicationDbContext _db;
-        public CompanyProfileRepository(ApplicationDbContext db)
+        public CompanyProfileRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
-
-        public async Task CreateAsync(CompanyProfile entity)
-        {
-            await _db.CompanyProfiles.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task<CompanyProfile> GetAsync(Expression<Func<CompanyProfile, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<CompanyProfile> companyProfiles = _db.CompanyProfiles;
-            if (!tracked)
-            {
-                companyProfiles = companyProfiles.AsNoTracking();
-            }
-            if (filter != null)
-            {
-                companyProfiles = companyProfiles.Where(filter);
-            }
-            return await companyProfiles.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<CompanyProfile>> GetAllAsync(Expression<Func<CompanyProfile, bool>> filter = null)
-        {
-            IQueryable<CompanyProfile> companyProfiles = _db.CompanyProfiles;
-            if (filter != null)
-            {
-                companyProfiles = companyProfiles.Where(filter);
-            }
-            return await companyProfiles.ToListAsync();
-        }
-
-        public async Task RemoveAsync(CompanyProfile entity)
-        {
-            _db.CompanyProfiles.Remove(entity);
-            await SaveAsync();
-        }
-        public async Task UpdateAsync(CompanyProfile entity)
+        public async Task<CompanyProfile> UpdateAsync(CompanyProfile entity)
         {
             _db.CompanyProfiles.Update(entity);
-            await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
             await _db.SaveChangesAsync();
+            return entity;
         }
     }
 }
--- Repository/IRepository/ICompanyProfileRepository.cs
+++ Repository/IRepository/ICompanyProfileRepository.cs
@@ -3,13 +3,8 @@ using System.Linq.Expressions;
 
 namespace HR_API.Repository.IRepository
 {
-    public interface ICompanyProfileRepository
+    public interface ICompanyProfileRepository : IRepository<CompanyProfile>
     {
-        Task<List<CompanyProfile>> GetAllAsync(Expression<Func<CompanyProfile, bool>> filter = null);
-        Task<CompanyProfile> GetAsync(Expression<Func<CompanyProfile, bool>> filter = null,bool tracked = true);
-        Task CreateAsync(CompanyProfile entity);
-        Task UpdateAsync(CompanyProfile entity);
-        Task RemoveAsync(CompanyProfile entity);
-        Task SaveAsync();
+        Task<CompanyProfile> UpdateAsync(CompanyProfile entity);
     }
 }
